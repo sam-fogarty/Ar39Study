678cb8d65a0376f4db7b1a94c93b9116d6080b56
Sam Fogarty <samuel.fogarty@colostate.edu>
2024-05-09 14:52:33 -0500

    Update Gallery version to work better with dunesw

--- pdune_setupGallery.sh
+++ pdune_setupGallery.sh
@@ -3,4 +3,5 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 
 setup dunesw v09_89_01d01 -q e26:prof 
-setup gallery v1_22_03 -q e26:prof
+setup gallery v1_22_06 -q e26:prof
+# if changing versions, check what versions work with each other by checking dependencies with ups depend <packagename> <version> -q <qualifier>
